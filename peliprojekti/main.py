e28e5050ee71ef40de6590fe798f7cb2fcbd0bfb
leevinyr <leevinyr@metropolia.fi>
2026-08-26 13:53:28 +0300

    peliprojekti 3

--- peliprojekti/main.py
+++ peliprojekti/main.py
@@ -1,31 +1,57 @@
 import sys
 
-nimi = input("Anna nimesi: ")
-ika = int(input("Anna ikäsi: "))
+ostoslista = []
 
-if(ika < 12):
-    sys.exit("Sinun on oltava vähintään 12 vuotias, jotta voit pelata.")
-else:
-    print("Tervetuloa,", nimi + ",", ika)
+def muuta_nimi():
+    nimi = input("Anna nimesi: ")
+    return nimi
+
+def muuta_ika():
+    ika = int(input("Anna ikäsi: "))
+    if(ika < 12):
+        sys.exit("Sinun on oltava vähintään 12 vuotias, jotta voit pelata.")
+    else:
+        return ika
+
+def lisaa_ostoslistaan():
+    while(True):
+        asia = input("Lisää asia ostoslistaan (tyhjä lopettaa): ")
+        if(asia == ""):
+            break
+        else:
+            ostoslista.append(asia)
+
+def nayta_ostoslista():
+    for i in ostoslista:
+        print(i)
+
+def lopeta():
+    sys.exit()
+
+muuta_nimi()
+muuta_ika()
 
 while(True):
     print("(1) Syötä nimi uudelleen")
     print("(2) Syötä ikä uudelleen")
-    print("(3) Lopeta")
+    print("(3) Lisää asioita ostoslistaan")
+    print("(4) Näytä ostoslista")
+    print("(0) Lopeta")
+
     valinta = input("Valitse komento: ")
+
     if(valinta == "1" or valinta.lower() == "syötä nimi uudelleen"):
-        nimi = input("Anna uusi nimi: ")
-        print("Tervetuloa,", nimi)
+        print(f"Tervetuloa, {muuta_nimi()}")
         continue    
     elif(valinta == "2" or valinta.lower() == "syötä ikä uudelleen"):
-        ika = int(input("Anna uusi ikä: "))
-        if(ika < 12):
-            sys.exit("Sinun on oltava vähintään 12 vuotias, jotta voit pelata.")
-        else:
-            print("Ikä", ika, "tallennettu")
-            continue
-    elif(valinta == "3" or valinta == "lopeta"):
-        sys.exit()
+        print(f"Ikä {muuta_ika()} tallennettu.")
+        continue
+    elif(valinta == "3" or valinta.lower() == "lisää asioita ostoslistaan"):
+        lisaa_ostoslistaan()
+    elif(valinta == "4" or valinta.lower() == "näytä ostoslista"):
+        nayta_ostoslista()
+    elif(valinta == "0" or valinta == "lopeta"):
+        lopeta()
     else:
         print("Virheellinen komento.")
         continue
